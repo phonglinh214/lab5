982426181951ab0fbf7a4d1dc8caf9eaeb30b4e1
Tran Minh Quan <quantmgch200753@fpt.edu.vn>
2022-09-24 17:01:39 +0700

    ok

---
+++ index.js
@@ -0,0 +1,31 @@
+//khai báo module express framework
+const express = require("express");
+const app = express();
+//khai báo module path
+const path = require("path");
+
+//khai báo server port
+const port = process.env.PORT || 3000;
+
+//set thư mục chứa view
+app.set("view", path.join(__dirname, "views"));
+
+//set view engine (template engine)
+//=> giúp gõ code của back-end ngay trong front-end dễ hơn
+//hbs: handle bar => nếu dùng view engine khác thì thay đổi giá trị này
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+    app.render("index");
+});
+
+app.get("/mobile", (req, res) => {
+    app.render("mobile");
+});
+
+app.get("/laptop", (req, res) => {
+    app.render("laptop");
+});
+
+//listen port để chạy server 
+app.listen(port)
